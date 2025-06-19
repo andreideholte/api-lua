f25efed13fa6602b0ba1578d5eb961330de58b85
Andrei Deholte <andreideholte@gmail.com>
2025-06-19 13:43:32 -0300

    chore: add log

--- app/app.py
+++ app/app.py
@@ -29,6 +29,8 @@ def create_map(chartType):
     if chartType not in ['Natal'] : 
         return jsonify({"error": "Sorry, the 'chartType' parameter is not supported yet."}), 404
 
+    print(request.json)
+    
     personal_data = request.json['data']
 
     name = personal_data['field:comp-mbwmkwa1']
