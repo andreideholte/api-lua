d3f5a906ece0dea3f2198481dd1eb1992c425713
Andrei Deholte <andreideholte@gmail.com>
2025-08-24 17:27:49 -0300

    fix: adjust regex translation

--- app/app.py
+++ app/app.py
@@ -90,7 +90,7 @@ def create_map(chartType):
     
     # Pós-processamento do SVG para remover "Pontos para <nome>"
     template = re.sub(r'<text[^>]*>Pontos para [^<]*</text>', '', template)
-    template = re.sub(r'<text[^>]*>Saturn[^<]*</text>', 'Saturno', template)
+    template = re.sub(r'(<text[^>]*>)Saturn([^<]*)(</text>)', r'\1Saturno\3', template)
     
     return jsonify({'chart': template})
 
