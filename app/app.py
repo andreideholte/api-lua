25246a88400f8f1166ebc851c8ce105cdee1da66
Andrei Deholte <andreideholte@gmail.com>
2025-08-24 16:01:57 -0300

    fix: replace regex saturn translate

--- app/app.py
+++ app/app.py
@@ -90,7 +90,7 @@ def create_map(chartType):
     
     # Pós-processamento do SVG para remover "Pontos para <nome>"
     template = re.sub(r'<text[^>]*>Pontos para [^<]*</text>', '', template)
-    template = re.sub('Saturn_comp-mees4pd8o', 'Saturno', template)
+    template = re.sub(r'Saturn\w*', 'Satuno', template)
     
     return jsonify({'chart': template})
 
